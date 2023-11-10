81ad75795139cd89ed94e7c9953598449e51cfc1
Truc Luong <truc.lng168@gmail.com>
2023-11-10 16:41:47 +1100

    date and time event selection

--- src/screens/events/EventPlan.js
+++ src/screens/events/EventPlan.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 import { theme, progressStyles } from '../../styles/Theme';
 import { flowStyles } from '../../styles/FlowStyles';
-import { PaperProvider } from 'react-native-paper';
-import {  View } from 'react-native';
+import { PaperProvider, Text } from 'react-native-paper';
+import { Image, View } from 'react-native';
 import TitleTopBar from '../../components/TitleTopBar';
 import WarningAlert from '../../components/Alert';
 import { ProgressStep, ProgressSteps } from 'react-native-progress-steps';
+import { CalendarList } from 'react-native-calendars';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 // All screens related to finer event details planning such as
 // date, time and optional activity and location selection
+
+const calendarTheme = {
+  textSectionTitleColor: theme.colors.text,
+  dayTextColor: theme.colors.text,
+  todayTextColor: theme.colors.success,
+  monthTextColor: theme.colors.text,
+  textMonthFontWeight: '500',
+};
 
 const NewEventPlan = ({ route, navigation }) => {
   const { eventId } = route.params ?? {};
@@ -21,6 +32,27 @@ const NewEventPlan = ({ route, navigation }) => {
     closeAlert();
     navigation.navigate('Events');
   }
+
+  // Dates selection
+  // Object.keys(markedDates) -> to extract selected dates
+  const [markedDates, setMarkedDates] = useState({});
+  const onDayPress = (day) => {
+    const selectedDate = day.dateString;
+    const updatedDates = { ...markedDates };
+    updatedDates[selectedDate] = {
+      ...markedDates[selectedDate],
+      selected: !markedDates[selectedDate]?.selected,
+      selectedColor: theme.colors.background,
+      selectedTextColor: theme.colors.primary,
+    };
+    setMarkedDates(updatedDates);
+  }
+
+  // Time selection
+  const [fromTime, setFromTime] = useState(new Date());
+  const [toTime, setToTime] = useState(new Date());
+  const fromTimeHandler = (e, selected) => setFromTime(selected);
+  const toTimeHandler = (e, selected) => setToTime(selected);
 
   return (
     <PaperProvider theme={theme}>
@@ -40,7 +72,50 @@ const NewEventPlan = ({ route, navigation }) => {
             label='Event Date'
             nextBtnTextStyle={{ color: theme.colors.text }}
           >
-
+            <View style={{ alignItems: 'center' }}>
+              <View
+                style={[flowStyles.outerSpeech, {
+                  marginTop: '5%'
+                }]}
+              >
+                <View
+                  style={[flowStyles.speechContainer, {
+                    width: 200,
+                    marginRight: '15%',
+                  }]}
+                >
+                  <Text
+                    variant='bodyLarge'
+                    style={{
+                      color: theme.colors.text,
+                    }}
+                  >
+                    What days would you like to meet on?
+                  </Text>
+                </View>
+              </View>
+              <View
+                style={[flowStyles.imageContainer, {
+                  marginLeft: '22%',
+                  marginBottom: '2%'
+                }]}
+              >
+                <Image 
+                  source={require('../../assets/wave.png')}
+                  style={flowStyles.imageStyle}
+                />
+              </View>
+              <CalendarList 
+                pastScrollRange={0}
+                futureScrollRange={12}
+                horizontal={true}
+                pagingEnabled={true}
+                minDate={moment().format('YYYY-MM-DD')}
+                markedDates={markedDates}
+                onDayPress={onDayPress}
+                theme={calendarTheme}
+              />
+            </View>
           </ProgressStep>
           <ProgressStep
             label='Event Time'
@@ -48,7 +123,82 @@ const NewEventPlan = ({ route, navigation }) => {
             previousBtnText='Back'
             previousBtnTextStyle={{ color: theme.colors.text }}
           >
-
+            <View style={{ alignItems: 'center' }}>
+              <View
+                style={[flowStyles.outerSpeech, {
+                  marginTop: '25%'
+                }]}
+              >
+                <View
+                  style={[flowStyles.speechContainer, {
+                    width: 200,
+                    marginRight: '13%'
+                  }]}
+                >
+                  <Text
+                    variant='bodyLarge'
+                    style={{
+                      color: theme.colors.text,
+                    }}
+                  >
+                    What times would you like to meet between?
+                  </Text>
+                </View>
+              </View>
+              <View
+                style={[flowStyles.imageContainer, {
+                  marginLeft: '13%'
+                }]}
+              >
+                <Image 
+                  source={require('../../assets/wave.png')}
+                  style={flowStyles.imageStyle}
+                />
+              </View>
+              <View
+                style={{
+                  width: 130,
+                  marginTop: '5%',
+                }}
+              >
+                <Text
+                  variant='bodyLarge'
+                  style={{
+                    color: theme.colors.text,
+                    marginBottom: '2%',
+                  }}
+                >
+                  From:
+                </Text>
+                <DateTimePicker 
+                  mode='time'
+                  value={fromTime}
+                  onChange={fromTimeHandler}
+                />
+              </View>
+              <View
+                style={{
+                  width: 130,
+                  marginTop: '2%',
+                }}
+              >
+                <Text
+                  variant='bodyLarge'
+                  style={{
+                    color: theme.colors.text,
+                    marginBottom: '2%',
+                    marginTop: '5%',
+                  }}
+                >
+                  To:
+                </Text>
+                <DateTimePicker 
+                  mode='time'
+                  value={toTime}
+                  onChange={toTimeHandler}
+                />
+              </View>
+            </View>
           </ProgressStep>
           <ProgressStep
             label='Activity'
