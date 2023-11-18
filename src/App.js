9ee01e1b6d12940bd7d7e4251659501a5a1ff7c3
Ivina <z5311943@ad.unsw.edu.au>
2023-11-18 14:08:49 +1100

    bottom nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './screens/Home';
 import Create from './screens/Create';
 import FriendsHome from './screens/FriendsHome';
+import Loading from './screens/Loading';
 import Profile from './screens/Profile';
 import { GroupRoutes } from './routes/GroupRoutes';
 import { EventRoutes } from './routes/EventRoutes';
@@ -12,6 +13,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View } from 'react-native';
+import { getCurrentUser } from './services/StoreService';
 
 const Tab = createMaterialBottomTabNavigator();
 const RootStack = createNativeStackNavigator();
@@ -33,58 +35,84 @@ const CreatePlaceholder = () => {
   );
 }
 
+const BottomBar = () => (
+  <PaperProvider theme={themeExtended}>
+    <Tab.Navigator
+      initialRouteName='Events'
+      activeColor='#FF8300'
+      inactiveColor='#9E9E9E'
+      barStyle={{ backgroundColor: '#F5F5F5' }}
+      screenOptions={{ headerShown: false }}
+      theme={themeExtended}
+    >
+      <Tab.Screen
+        name='Events'
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='home' color={color} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Create'
+        component={CreatePlaceholder}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='plus' color={color} size={30} />
+          ),
+        }}
+        listeners={({ navigation }) => ({
+          tabPress: event => {
+            event.preventDefault();
+            navigation.navigate('CreateNew');
+          }
+        })}
+      />
+      <Tab.Screen
+        name='Friends'
+        component={FriendsHome}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='account-multiple' color={color} size={30} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  </PaperProvider>
+);
+
 const App = () => {
-  const BottomBar = () => (
-    <PaperProvider theme={themeExtended}>
-      <Tab.Navigator
-        initialRouteName='Events'
-        activeColor='#FF8300'
-        inactiveColor='#9E9E9E'
-        barStyle={{ backgroundColor: '#F5F5F5' }}
-        screenOptions={{ headerShown: false }}
-        theme={themeExtended}
-      >
-        <Tab.Screen
-          name='Events'
-          component={Home}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='home' color={color} size={30} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Create'
-          component={CreatePlaceholder}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='plus' color={color} size={30} />
-            ),
-          }}
-          listeners={({ navigation }) => ({
-            tabPress: event => {
-              event.preventDefault();
-              navigation.navigate('CreateNew');
-            }
-          })}
-        />
-        <Tab.Screen
-          name='Friends'
-          component={FriendsHome}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='account-multiple' color={color} size={30} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </PaperProvider>
-  );
+  const [startingScreen, setStartingScreen] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchCurrentUser = async () => {
+    try {
+      const user = await getCurrentUser();
+      // if user is null, then no user is logged in. otherwise, user is logged in
+      if (user === null) {
+        setStartingScreen('LoginRoutes');
+      } else {
+        setStartingScreen('Bottom Tab Bar');
+      }
+      setIsLoading(false);
+    } catch (e) {
+      console.error('Error fetching current user:', e);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
+
+  // loading when setStartingScreen is still waiting to finish running
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
-      <RootStack.Navigator
-        initialRouteName='LoginRoutes'
-      >
+      <RootStack.Navigator initialRouteName={startingScreen}>
         <RootStack.Screen
           name='Bottom Tab Bar'
           component={BottomBar}
