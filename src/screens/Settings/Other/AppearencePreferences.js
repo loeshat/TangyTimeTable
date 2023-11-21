73f6d90e04f6c87bd8dd589dbf4dd2379f0019fc
Olivia Chen <olivia.a.chen.oac@gmail.com>
2023-11-22 02:03:13 +1100

    adding hints for apperance preferences

--- src/screens/Settings/Other/AppearencePreferences.js
+++ src/screens/Settings/Other/AppearencePreferences.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import TitleTopBarDark from "../../../components/TitleTopBarDark";
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, Button } from "react-native-paper";
 import { theme, invertedTheme } from "../../../styles/Theme";
-import { View, Text, Switch, StyleSheet } from "react-native";
+import { View, Text, Switch, StyleSheet, TouchableOpacity } from "react-native";
+import WarningAlert from '../../../components/Alert';
 
 /**
  * Display for user to view and change appearance preferences
@@ -15,6 +16,19 @@ const AppearencePreferences = ({ navigation }) => {
   const [highContrastMode, setHighContrastMode] = useState(false);
   const [gifsAndAnimations, setGifsAndAnimations] = useState(true);
   const [displayTheme, setDisplayTheme] = useState(theme);
+
+  // Handle warning alert for tipbits: Dark Mode
+  const [alertDark, setAlertDark] = useState(false);
+  const displayAlertDark = () => setAlertDark(true);
+  const closeAlertDark = () => setAlertDark(false);
+  // Handle warning alert for tipbits: High Contrast Mode
+  const [alertHighContrast, setAlertHighContrast] = useState(false);
+  const displayAlertHighContrast = () => setAlertHighContrast(true);
+  const closeAlertHighContrast = () => setAlertHighContrast(false);
+  // Handle warning alert for tipbits: Gifs and Animations
+  const [alertAnimations, setAlertAnimations] = useState(false);
+  const displayAlertAnimations = () => setAlertAnimations(true);
+  const closeAlertAnimations = () => setAlertAnimations(false);
 
   // When High Contrast is toggled, change the theme accordingly
   useEffect(() => {
@@ -39,7 +53,7 @@ const AppearencePreferences = ({ navigation }) => {
   };
 
   // The toggle component that adapts view based on the state of the field
-  const OptionToggle = ({ label, value, onToggle }) => {
+  const OptionToggle = ({ label, value, onToggle, displayAlert }) => {
     return (
       <View
         style={[
@@ -48,14 +62,17 @@ const AppearencePreferences = ({ navigation }) => {
           { backgroundColor: value ? displayTheme.colors.APbuttonOn : displayTheme.colors.APbuttonOff },
         ]}
       >
-        <Text
-          style={[
-            styles.optionText,
-            { color: value? displayTheme.colors.APtextOn : displayTheme.colors.APtextOff, fontWeight: value ? "bold" : "normal" },
-          ]}
-        >
-          {label}
-        </Text>
+        <TouchableOpacity onPress={displayAlert ? displayAlert : null}>
+          <Text
+            style={[
+              styles.optionText,
+              { color: value? displayTheme.colors.APtextOn : displayTheme.colors.APtextOff, fontWeight: value ? "bold" : "normal" },
+            ]}
+          >
+            {label}<Button icon={'comment-question'} />
+          </Text>
+        </TouchableOpacity>
+        
         <Switch
           value={value}
           onValueChange={onToggle}
@@ -80,6 +97,35 @@ const AppearencePreferences = ({ navigation }) => {
           darkTheme={false}
         />
       }
+
+      <WarningAlert
+        description={`Dark mode offers a visually comfortable experience in low-light settings, appealing to users who prefer a darker interface.`}
+        affirmText={'Ok'}
+        affirmAction={closeAlertDark}
+        affirmContentStyle={{ width: 125 }}
+        cancelAction={closeAlertDark}
+        closeAction={closeAlertDark}
+        visible={alertDark}
+      />
+      <WarningAlert
+        description={`High contrast enhances visibility by emphasizing color contrast, benefiting users who require or prefer a more distinct and easily readable interface.`}
+        affirmText={'Ok'}
+        affirmAction={closeAlertHighContrast}
+        affirmContentStyle={{ width: 125 }}
+        cancelAction={closeAlertHighContrast}
+        closeAction={closeAlertHighContrast}
+        visible={alertHighContrast}
+      />
+
+      <WarningAlert
+        description={`GIFs and animations boost engagement with dynamic visuals, catering to users who prefer interactive and visually stimulating content.`}
+        affirmText={'Ok'}
+        affirmAction={closeAlertAnimations}
+        affirmContentStyle={{ width: 125 }}
+        cancelAction={closeAlertAnimations}
+        closeAction={closeAlertAnimations}
+        visible={alertAnimations}
+      />
         
       <View style={highContrastMode ? styles.contentContainerDark : styles.contentContainer}>
         <Text style={highContrastMode ? styles.titleDark : styles.title}>Update Appearence Preferences</Text>
@@ -87,18 +133,21 @@ const AppearencePreferences = ({ navigation }) => {
           label="Dark Mode"
           value={darkMode}
           onToggle={() => toggleSwitch('darkMode')}
+          displayAlert={displayAlertDark}
         />
 
         <OptionToggle
           label="High Contrast Mode"
           value={highContrastMode}
           onToggle={() => toggleSwitch('highContrastMode')}
+          displayAlert={displayAlertHighContrast}
         />
 
         <OptionToggle
           label="Gifs and Animations"
           value={gifsAndAnimations}
           onToggle={() => toggleSwitch('gifsAndAnimations')}
+          displayAlert={displayAlertAnimations}
         />
       </View>
     </PaperProvider>
