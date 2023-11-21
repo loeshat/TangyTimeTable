d549d23f45a2e78da15e182e2d8a65d2d21df9bc
Truc Luong <truc.lng168@gmail.com>
2023-11-22 00:15:10 +1100

    ran prettier on profile and settingsroutes files

--- src/routes/SettingsRoutes.js
+++ src/routes/SettingsRoutes.js
@@ -1,20 +1,20 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ChangeSyncedCalendar from "../screens/Settings/Account/ChangeSyncedCalendar";
-import ChangeConnectedAccounts from "../screens/Settings/Account/ChangeConnectedAccounts";
-import ChangePassword from "../screens/Settings/Account/ChangePassword";
-import PushNotifications from "../screens/Settings/Notifications/PushNotifications";
-import ConsentAndPrivacy from "../screens/Settings/PrivacyAndData/ConsentAndPrivacy";
-import DataAndSharing from "../screens/Settings/PrivacyAndData/DataAndSharing";
-import Language from "../screens/Settings/Other/Language";
-import RegionAndTimezone from "../screens/Settings/Other/RegionAndTimezone";
-import TextSizeAndFont from "../screens/Settings/Other/TextSizeAndFont";
-import AppearencePreferences from "../screens/Settings/Other/AppearencePreferences";
-import DataAndCache from "../screens/Settings/Other/DataAndCache";
-import HelpAndSupport from "../screens/Settings/Support/HelpAndSupport";
-import TermsAndPolicies from "../screens/Settings/Support/TermsAndPolicies";
-import AboutUs from "../screens/Settings/Support/AboutUs";
-import ReportAProblem from "../screens/Settings/Support/ReportAProblem";
-import UpdatesAndReleaseNotes from "../screens/Settings/Support/UpdatesAndReleaseNotes";
+import ChangeSyncedCalendar from '../screens/Settings/Account/ChangeSyncedCalendar';
+import ChangeConnectedAccounts from '../screens/Settings/Account/ChangeConnectedAccounts';
+import ChangePassword from '../screens/Settings/Account/ChangePassword';
+import PushNotifications from '../screens/Settings/Notifications/PushNotifications';
+import ConsentAndPrivacy from '../screens/Settings/PrivacyAndData/ConsentAndPrivacy';
+import DataAndSharing from '../screens/Settings/PrivacyAndData/DataAndSharing';
+import Language from '../screens/Settings/Other/Language';
+import RegionAndTimezone from '../screens/Settings/Other/RegionAndTimezone';
+import TextSizeAndFont from '../screens/Settings/Other/TextSizeAndFont';
+import AppearencePreferences from '../screens/Settings/Other/AppearencePreferences';
+import DataAndCache from '../screens/Settings/Other/DataAndCache';
+import HelpAndSupport from '../screens/Settings/Support/HelpAndSupport';
+import TermsAndPolicies from '../screens/Settings/Support/TermsAndPolicies';
+import AboutUs from '../screens/Settings/Support/AboutUs';
+import ReportAProblem from '../screens/Settings/Support/ReportAProblem';
+import UpdatesAndReleaseNotes from '../screens/Settings/Support/UpdatesAndReleaseNotes';
 
 const Root = createNativeStackNavigator();
 
@@ -22,93 +22,92 @@ const Root = createNativeStackNavigator();
  * Stores settings related routes for all Settings screens
  * which cannot be accessed by clicking on the bottom
  * navigation bar
- * @returns 
+ * @returns
  */
 
 export const SettingsRoutes = () => {
-    return (
-        <Root.Navigator>
-            <Root.Screen
-                name='ChangeSyncedCalendar'
-                component={ChangeSyncedCalendar}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='ChangeConnectedAccounts'
-                component={ChangeConnectedAccounts}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='ChangePassword'
-                component={ChangePassword}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='PushNotifications'
-                component={PushNotifications}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='ConsentAndPrivacy'
-                component={ConsentAndPrivacy}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='DataAndSharing'
-                component={DataAndSharing}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='Language'
-                component={Language}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='RegionAndTimezone'
-                component={RegionAndTimezone}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='TextSizeAndFont'
-                component={TextSizeAndFont}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='AppearancePreferences'
-                component={AppearencePreferences}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='DataAndCache'
-                component={DataAndCache}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='HelpAndSupport'
-                component={HelpAndSupport}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='TermsAndPolicies'
-                component={TermsAndPolicies}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='AboutUs'
-                component={AboutUs}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='ReportAProblem'
-                component={ReportAProblem}
-                options={{ headerShown: false }}
-            />
-            <Root.Screen
-                name='UpdatesAndReleaseNotes'
-                component={UpdatesAndReleaseNotes}
-                options={{ headerShown: false }}
-            />
-        </Root.Navigator>
-    );
-}
-
+	return (
+		<Root.Navigator>
+			<Root.Screen
+				name='ChangeSyncedCalendar'
+				component={ChangeSyncedCalendar}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='ChangeConnectedAccounts'
+				component={ChangeConnectedAccounts}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='ChangePassword'
+				component={ChangePassword}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='PushNotifications'
+				component={PushNotifications}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='ConsentAndPrivacy'
+				component={ConsentAndPrivacy}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='DataAndSharing'
+				component={DataAndSharing}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='Language'
+				component={Language}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='RegionAndTimezone'
+				component={RegionAndTimezone}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='TextSizeAndFont'
+				component={TextSizeAndFont}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='AppearancePreferences'
+				component={AppearencePreferences}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='DataAndCache'
+				component={DataAndCache}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='HelpAndSupport'
+				component={HelpAndSupport}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='TermsAndPolicies'
+				component={TermsAndPolicies}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='AboutUs'
+				component={AboutUs}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='ReportAProblem'
+				component={ReportAProblem}
+				options={{ headerShown: false }}
+			/>
+			<Root.Screen
+				name='UpdatesAndReleaseNotes'
+				component={UpdatesAndReleaseNotes}
+				options={{ headerShown: false }}
+			/>
+		</Root.Navigator>
+	);
+};
